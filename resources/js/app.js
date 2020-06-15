8d133b0606d88868ca700bb494c071f22e385deb
shimadakouhei <shimapykkm@gmail.com>
2020-06-16 06:23:10 +0900

    Update app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,2 +1,15 @@
+/**
+ * First we will load all of this project's JavaScript dependencies which
+ * includes Vue and other libraries. It is a great starting point when
+ * building robust, powerful web applications using Vue and Laravel.
+ */
+
 require('./bootstrap');
-require('./users')
+
+require('./users');
+
+require('./jquery.jTinder'); //追加
+
+require('./jquery.transform2d'); //追加
+
+require('./jTinder'); //追加
